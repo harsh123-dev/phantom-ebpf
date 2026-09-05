f2d21ecf143abb470ca56ea5a2f54037e7f9701d
Harsh <harshranjan363@gmail.com>
2026-09-05 16:54:27 +0530

    Include ebpf-agent-daemonset in bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -75,6 +75,7 @@ kubectl create secret generic phantom-agent-secret -n phantom \
 
 echo "[6/6] Deploying Microservices & Evaluation Targets..."
 kubectl apply -f infra/k8s/api-gateway-deployment.yaml
+kubectl apply -f infra/k8s/ebpf-agent-daemonset.yaml
 kubectl apply -f research/evaluation/k8s/target-workloads.yaml
 
 echo ""
